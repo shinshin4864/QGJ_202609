f095eebbebf3d58a6b09de4fff5d54a852715012
shinshin4864 <kkst5963@gmail.com>
2026-09-15 18:56:51 +0900

    startingなど

--- Assets/Scripts/ScoreSystem.cs
+++ Assets/Scripts/ScoreSystem.cs
@@ -1,25 +1,85 @@
 using UnityEngine;
 using TMPro;
+using System.Collections.Generic;
 
 public class ScoreSystem : MonoBehaviour
 {
     float score = 0f;
     float money = 0f;
-    TextMeshProUGUI Scores;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    float star = 0f;
+
+    // ランクごとの報酬金額を定義
+    Dictionary<int, int> ranktomoney = new Dictionary<int, int>()
     {
+        {1, 100},
+        {2, 200},
+        {3, 300},
+        {4, 400},
+        {5, 500}
+    };
+    // 調理完了時に呼び出し
+    public void chackscore(int rank, float order_time)
+    {
+
+        AddScore(rank);
+        AddMoney(rank);
+        RemoveScore(rank, order_time);
+    }
+    void AddScore(int rank)
+    {
+        score += rank;
+        PlayerPrefs.SetFloat("score", score);
+        PlayerPrefs.Save(); 
+
         
+        Judgescore(score);
     }
 
-    // Update is called once per frame
-    void Update()
+    void AddMoney(int rank)
     {
-        
+        if (ranktomoney.ContainsKey(rank))
+        {
+            money += ranktomoney[rank];
+            PlayerPrefs.SetFloat("money", money);
+            PlayerPrefs.Save(); 
+        }
+        else
+        {
+            Debug.LogWarning("存在しないランクです: " + rank);
+        }
     }
-    void AddScore(float amount)
+    void RemoveScore(int rank, float order_time)
     {
-        score += amount;
+        score -= rank;
+        if (score < 0) score = 0; // スコアが負にならないようにする
         PlayerPrefs.SetFloat("score", score);
+        PlayerPrefs.Save(); 
+    }
+    //スコアから星の数を判定する関数
+    void Judgescore(float currentScore)
+    {
+        if (currentScore >= 1000)
+        {
+            star = 3f;
+        }
+        else if (currentScore >= 800)
+        {
+            star = 2.5f;
+        }
+        else if (currentScore >= 600)
+        {
+            star = 2f;
+        }
+        else if (currentScore >= 400)
+        {
+            star = 1f;
+        }
+        else
+        {
+            star = 0.5f;
+        }
+
+        PlayerPrefs.SetFloat("star", star);
+        PlayerPrefs.Save();
     }
 }
--- Assets/Scripts/Starting.cs
+++ Assets/Scripts/Starting.cs
@@ -1,23 +1,97 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using TMPro;
 
-public class Starting : MonoBehaviour, IPointerClickHandler
+public class Starting : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("UI要素の参照")]
+    [SerializeField] private Button firstButton;    // 1番上のボタン（切替用）
+    [SerializeField] private Button secondButton1;   // 2番目のボタン
+    [SerializeField] private Button thirdButton2;   // 3番目のボタン
+
+    [SerializeField] private TextMeshProUGUI firstButtonText;
+    [SerializeField] private TextMeshProUGUI secondButtonText;
+    [SerializeField] private TextMeshProUGUI thirdButtonText;
+private int currentMode = 0;
+
     void Start()
     {
-        Debug.Log("Starting");
+        // ボタンがクリックされたときの処理をコード側で登録
+        firstButton.onClick.AddListener(OnfirstButtonClicked);
+        secondButton1.onClick.AddListener(OnsecondButtonClicked);
+        thirdButton2.onClick.AddListener(OnthirdButtonClicked);
+
+        // 最初の状態をセット
+        UpdateUI();
     }
 
-    // Update is called once per frame
-    void Update()
+    // 1番上のボタンを押したとき（モード切替）
+    void OnfirstButtonClicked()
     {
-        
+        if (currentMode == 0) {
+            currentMode = (currentMode + 1) % 2;
+        }
+        else
+        {
+            PlayerPrefs.SetInt("difficulty", 1);
+            SceneManager.LoadScene("Game");
+        }
+        Debug.Log("モードを切り替えました: " + currentMode);
+        UpdateUI();
     }
-    public void OnPointerClick(PointerEventData eventData)
+
+    // 見た目（テキスト）を現在のモードに合わせて更新
+    void UpdateUI()
     {
-        Debug.Log(gameObject.name + "was clicked.");
-        SceneManager.LoadScene("Game");
+        if (currentMode == 0)
+        {
+            firstButtonText.text = "スタート";
+            secondButtonText.text = "チュートリアル";
+            thirdButtonText.text = "終了";
+        }
+        else if (currentMode == 1)
+        {
+            firstButtonText.text = "イージー";
+            secondButtonText.text = "ノーマル";
+            thirdButtonText.text = "ハード";
+        }
+    }
+
+    // 2番目のボタンを押したときの実装内容
+    void OnsecondButtonClicked()
+    {
+        if (currentMode == 0)
+        {
+            SceneManager.LoadScene("Tutorial");
+        }
+        else if (currentMode == 1)
+        {
+            PlayerPrefs.SetInt("difficulty", 2);
+            SceneManager.LoadScene("Game");
+        }
+    }
+
+    // 3番目のボタンを押したときの実装内容
+    void OnthirdButtonClicked()
+    {
+        if (currentMode == 0)
+        {
+            QuitGame();
+        }
+        else if (currentMode == 1)
+        {
+            PlayerPrefs.SetInt("difficulty", 3);
+            SceneManager.LoadScene("Game");
+        }
+    }
+    void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false; // エディタ実行時は停止
+#else
+        Application.Quit(); // 本番ビルド時はアプリ終了
+#endif
     }
 }
