9cb7619d570a03af4df97a3494095722ca198382
shinshin4864 <kkst5963@gmail.com>
2026-09-16 20:30:42 +0900

    Tutorial

--- Assets/Scripts/Starting.cs
+++ Assets/Scripts/Starting.cs
@@ -9,24 +9,42 @@ public class Starting : MonoBehaviour
 {
     //難易度の設定(1=easy,2=normal,3=hard)
     public static int difficulty;
+
     private TextMeshProUGUI TextMeshPro;
     private AudioSource audiosSource;
     public AudioClip titleBGM;
+
+    [SerializeField] private GameObject Slideshow;
+
     [SerializeField] private GameObject fourth;
+
     [Header("UI要素の参照")]
     [SerializeField] private Button firstButton;    // 1番上のボタン（切替用）
     [SerializeField] private Button secondButton1;   // 2番目のボタン
-    [SerializeField] private Button thirdButton2;   // 3番目のボタン
-    [SerializeField] private Button forthButton;    // 4番目のボタン
+    [SerializeField] private Button thirdButton2;    // 3番目のボタン
+    [SerializeField] private Button forthButton;     // 4番目のボタン
 
     [SerializeField] private TextMeshProUGUI firstButtonText;
     [SerializeField] private TextMeshProUGUI secondButtonText;
     [SerializeField] private TextMeshProUGUI thirdButtonText;
     [SerializeField] private TextMeshProUGUI forthButtonText;
-    private int currentMode = 0;
 
-    void Start()
+    private int currentMode = 0;
+    private List<GameObject> TitleObjects = new List<GameObject>();
+
+    private void RegistTitleObjects()
     {
+        GameObject[] titleObjects = GameObject.FindGameObjectsWithTag("Title");
+        foreach (GameObject obj in titleObjects)
+        {
+            TitleObjects.Add(obj);
+        }
+    }
+
+    private void Start()
+    {
+        RegistTitleObjects();
+
         // ボタンがクリックされたときの処理をコード側で登録
         firstButton.onClick.AddListener(OnfirstButtonClicked);
         secondButton1.onClick.AddListener(OnsecondButtonClicked);
@@ -35,30 +53,9 @@ public class Starting : MonoBehaviour
 
         // 最初の状態をセット
         UpdateUI();
-
-        // AudioSourceコンポーネントを取得
-        audiosSource = GetComponent<AudioSource>();
-        //audiosSource.clip = titleBGM;
-        audiosSource.Play();
     }
 
-    // 1番上のボタンを押したとき（モード切替）
-    void OnfirstButtonClicked()
-    {
-        if (currentMode == 0) {
-            currentMode = (currentMode + 1) % 2;
-        }
-        else
-        {
-            difficulty = 1;
-            SceneManager.LoadScene("Game");
-        }
-        Debug.Log("モード切り替え: " + currentMode);
-        UpdateUI();
-    }
-
-    
-    void UpdateUI()
+    private void UpdateUI()
     {
         if (currentMode == 0)
         {
@@ -77,12 +74,29 @@ public class Starting : MonoBehaviour
         }
     }
 
-    // 2番目のボタンを押したときの実装内容
-    void OnsecondButtonClicked()
+    // 1番上のボタンを押したとき（モード切替）
+    private void OnfirstButtonClicked()
     {
         if (currentMode == 0)
         {
-            SceneManager.LoadScene("Tutorial");
+            currentMode = (currentMode + 1) % 2;
+        }
+        else
+        {
+            difficulty = 1;
+            SceneManager.LoadScene("Game");
+        }
+
+        Debug.Log("モード切り替え: " + currentMode);
+        UpdateUI();
+    }
+
+    // 2番目のボタンを押したときの実装内容
+    private void OnsecondButtonClicked()
+    {
+        if (currentMode == 0)
+        {
+            ChangeTutorialMode();
         }
         else if (currentMode == 1)
         {
@@ -92,7 +106,7 @@ public class Starting : MonoBehaviour
     }
 
     // 3番目のボタンを押したときの実装内容
-    void OnthirdButtonClicked()
+    private void OnthirdButtonClicked()
     {
         if (currentMode == 0)
         {
@@ -104,24 +118,44 @@ public class Starting : MonoBehaviour
             SceneManager.LoadScene("Game");
         }
     }
-    void OnforthButtonClicked()
+
+    private void OnforthButtonClicked()
     {
         if (currentMode == 0)
         {
             return;
         }
-        else
-        {
-            currentMode = 0;
-            UpdateUI();
-        }
+
+        currentMode = 0;
+        UpdateUI();
     }
-    void QuitGame()
+
+    private void ChangeTutorialMode()
+    {
+        foreach (GameObject obj in TitleObjects)
+        {
+            obj.SetActive(false);
+        }
+
+        //Slideshow = GameObject.Find("Slideshow");
+        Slideshow.SetActive(true);
+        TutorialSlideshow tutorialSlideshow = Slideshow.GetComponent<TutorialSlideshow>();
+        tutorialSlideshow.StartCoroutine(tutorialSlideshow.Slideshow());
+    }
+    public void ChangeStartMode()
+    {
+        foreach (GameObject obj in TitleObjects)
+        {
+            obj.SetActive(true);
+        }
+        Slideshow.SetActive(false);
+    }
+    private void QuitGame()
     {
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false; // エディタ実行時は停止
 #else
-        Application.Quit(); 
+        Application.Quit();
 #endif
     }
 }
--- Assets/Scripts/TutorialSlideshow.cs
+++ Assets/Scripts/TutorialSlideshow.cs
@@ -5,21 +5,23 @@ using System.Collections;
 public class TutorialSlideshow : MonoBehaviour
 {
     [SerializeField] private Sprite[] SlideshowImages; // 背景画像の配列
-    private Image TSphoto; 
+    [SerializeField] private Image TSphoto; 
     [SerializeField] private float slideDuration = 2f; // 画像の表示時間
-    void Start()
+    private void Start()
     {
-        TSphoto = GetComponent<Image>();
-        StartCoroutine(slideshow());
+        //TSphoto = GetComponent<Image>();
+        //StartCoroutine(slideshow());
         
     }
-    IEnumerator slideshow()
+    public IEnumerator Slideshow()
     {
         for (int i = 0; i < SlideshowImages.Length; i++)
         {
             TSphoto.sprite = SlideshowImages[i]; // 画像を変更
             yield return new WaitForSeconds(slideDuration); // 画像の表示時間
         }
+        // スライドショーが終了したら、タイトル画面に戻る
+        GameObject.Find("Canvas").GetComponent<Starting>().ChangeStartMode();
     }
 }
 
